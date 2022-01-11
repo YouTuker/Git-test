35cfd368f50c24ce6dce0c24206989a0144f50cb
yangchuantang <layne@atguigu.com>
2022-01-11 14:19:54 +0800

    Git-Maven-second-master-commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git-Test");
         System.out.println("Git-test2");
+        System.out.println("master");
     }
 }
